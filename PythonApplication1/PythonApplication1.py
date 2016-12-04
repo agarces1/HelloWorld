7dba12d1d915675fa39d7e0c2b9c7d1c6d6e6bf9
adria <adria@ADRLT3>
2016-12-03 22:10:54 -0600

    Add project files.

---
+++ PythonApplication1/PythonApplication1.py
@@ -0,0 +1,10 @@
+#This is a comment;Always comment relevant things not everything 
+print("Hello World");
+print('Hello There');#Quotes must match.
+print("It's a beautiful day");#you can mix 'and " but two must match to close the string
+print("This is how you force a new line.\n make sure you put backward slash not foward");
+print("""Hey this is a  neat trick to print over multiple lines in the IDE""");
+print("Here is a string" + " and here how you treat them as one long string in the IDE ");
+print("Yes, you can print \ on the screen");
+print("If there is a slash on the string, put another slash so it can display E.I \\news");
+print("Hello");
